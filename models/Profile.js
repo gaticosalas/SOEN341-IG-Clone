e4add94ffbf031f3debe103da0f8e6b228d9345b
Juan Salas <juan.as.ga@gmail.com>
2021-02-09 12:21:34 -0500

    #6 [ADD] Profile Database Schema

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,55 @@
 const mongoose = require('mongoose');
 
-const ProfileSchema = new mongoose.Schema({});
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    location: {
+        type: String
+    },
+    bio: {
+        type: String
+    },
+    follows: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    followedBy: [
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+        },
+        name: {
+            type: String
+        },
+        avatar: {
+            type: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
 
 module.exports = User = mongoose.model('profile', ProfileSchema);
