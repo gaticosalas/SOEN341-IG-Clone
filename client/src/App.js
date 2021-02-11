7345363bd6a0348ff84159155408a8d7840e1331
Juan Salas <juan.as.ga@gmail.com>
2021-02-10 19:39:16 -0500

    #17 [CHANGE] Functional components instead of Class components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// Containers
-import Home from './containers/layout/Home';
-import Navbar from './containers/layout/Navbar';
-
 // Components
-import HomeComponent from './components/layout/home';
-import NavbarComponent from './components/layout/navbar';
+import Home from './components/layout/home';
+import Navbar from './components/layout/navbar';
 
 import './App.css';
 
 const App = () => (
   <Router>
     <Fragment>
-      <Navbar  Layout={NavbarComponent}/>
-      <Route exact path='/' component={(props) => <Home {...props} Layout={HomeComponent} title="Home"/>} />
+      <Navbar />
+      <Route exact path='/' component={Home} />
       <section className="container">
         <Switch>
           {/* Other routes will go here like such: */}
-          {/* <Route exact path='/' component={(props) => <Home {...props} Layout={HomeComponent} title="Home"/>} /> */}
+          {/* <Route exact path='/' component={HomeComponent} /> */}
         </Switch>
       </section>
     </Fragment>
--- client/src/components/layout/home.jsx
+++ client/src/components/layout/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Home = ({ title }) => {
-    document.title = title;
+const Home = (props) => {
+    document.title = "Home";
     
     return (
         <div>
