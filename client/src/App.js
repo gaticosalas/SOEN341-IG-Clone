0aa81d6edfab81932561e1babe62f531b2e4ac45
Juan Salas <juan.as.ga@gmail.com>
2021-02-10 19:54:06 -0500

    #19 [ADD] Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store'
+
 // Components
-import Home from './components/layout/home';
-import Navbar from './components/layout/navbar';
+import Home from './components/layout/Home';
+import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Home} />
-      <section className="container">
-        <Switch>
-          {/* Other routes will go here like such: */}
-          {/* <Route exact path='/' component={HomeComponent} /> */}
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            {/* Other routes will go here like such: */}
+            {/* <Route exact path='/' component={HomeComponent} /> */}
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 )
 export default App;
