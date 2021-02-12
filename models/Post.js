fe4ac36d72d412c702cf5f121dc645192c24dc07
loren589 <lorenbertacchi@gmail.com>
2021-02-12 18:45:27 -0500

    #7 [ADD] Post Database Schema

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,64 @@
 const mongoose = require('mongoose');
 
-const PostSchema = new mongoose.Schema({});
+const PostSchema = new mongoose.Schema({
+    //making the reference to the user
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+         ref: 'users' //referencing the user to the post -->shows who made the post
+        },
+    //including a text field for the post's caption
+    caption:{
+        type: String,
+        required: true //should a text field be mandatory though?
+    },
+    //the post user's name -->if the user deletes an account, the post can still remain
+    name: {
+        type: String
+    },
+    //the user's profile pic
+    avatar: {
+        type: String
+    },
+    //the 'likes' or in this case bookmarking -->storing them in an array
+    likes: [
+    {
+        //this will allow us to know which likes came from which users
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users'
+        }
+    }],
 
-module.exports = User = mongoose.model('post', PostSchema);
+    //date the post was published on
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    //comments on the post will be stored in this array
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            //profile pic
+            avatar: {
+                type: String
+            },
+            //date the comment was posted on
+            date: {
+                type: Date,
+                default: Date.now //current date gets put by default
+            }
+        }
+    ]
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
