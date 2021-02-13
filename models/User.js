110b22dacea4487d93be8550f174820ae68e0f9a
MKB <45188433+MKB-1@users.noreply.github.com>
2021-02-13 09:29:35 -0500

    #5 [ADD] User Database Schema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
+});
 
 module.exports = User = mongoose.model('user', UserSchema);
