54edeaaa1091290a76a35659bb8cc33fa95f4726
loren589 <lorenbertacchi@gmail.com>
2021-02-24 14:18:32 -0500

    testing out the follow/unfollow system

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,19 +1,28 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Redirect, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchProfile } from '../../actions/profile';
 
-const Profile = ({ isAuthenticated, me, fetchProfile, profile, user, isFetching }) => {
+
+const Profile = ({ isAuthenticated, me, fetchProfile, profile, isFetching }) => {
     // grabs route parameter (user_id) from '/profile/:user_id'
     let { user_id } = useParams();
+    const [isMe, setIsMe] = useState(false);
+    const [isFollowed, setIsFollowed] = useState(false);
 
     // useEffect with a [] as its second parameter executes the content
     // of the first function only once, when the component is mounted (first loaded).
     useEffect(() => {
         fetchProfile(user_id);
+
+        //IDs match so isMe is set as true, false if they don't
+        user_id === me ? setIsMe(true) : setIsMe(false);
+
+        //checking to see if 'me' is already following (in the followedBy array) the user who's profile we're checking out
+        me === user_id.followedBy ? setIsFollowed(true) : setIsFollowed(false);
+        
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
 
     // If user is not authenticated, redirected to login page (home)
     if (!isAuthenticated) {
@@ -24,18 +33,18 @@ const Profile = ({ isAuthenticated, me, fetchProfile, profile, user, isFetching 
     if (isFetching) {
         return <p>Loading...</p>
     }
-
-    if (!isFetching) {
+    
+    if(!isFetching) {
         // Grabbing all the values from the main objects.
         // Uncomment the following console.log to see the values of the profile object.
-        // console.log(profile);
-        const { user, bio, follows, followedBy } = profile;
-        const { avatar, first_name, last_name, username } = user;
-
+        //console.log(profile);
+        const { bio, user, follows, followedBy } = profile;
+        const { first_name, last_name, username, avatar } = user;
+        
         return (
             <Fragment>
                 <div className="userInfo">
-                    <img src={avatar} alt="user profile avatar" />
+                    <img src={avatar} alt="user profile avatar"/>
                     <div>
                         <p>{`Name: ${first_name} ${last_name}`}</p>
                     </div>
@@ -51,6 +60,19 @@ const Profile = ({ isAuthenticated, me, fetchProfile, profile, user, isFetching 
                     <div>
                         <p>{`Followed by ${followedBy.length} people`}</p>
                     </div>
+                    
+                    {isMe?
+                    //checking to see if a user is NOT on their own profile page, and if they are following/not following the profile they're checking out
+                    //the type & value should change depending on whether or not they are following/notfollowing the profile in question
+                        <div>
+                            <input type={isFollowed? "follow" : "unfollow"}
+                             className="btn btn-primary" 
+                             value={isFollowed? "followUser" : "unfollowUser"} />
+                        </div>
+                    :null
+                     
+                    }
+                     
                 </div>
             </Fragment>
         )
